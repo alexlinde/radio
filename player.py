2febe123af2645ca08b809bb202608dc74a3f1df
Alex Linde <alex@somefamilies.com>
2020-01-10 13:31:41 -0800

    use standard python logging

--- player.py
+++ player.py
@@ -2,13 +2,7 @@ import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import GObject, Gst, GLib
 from threading import Thread
-
-DEBUG = True
-
-def debug(str, end='\n'):
-    if not DEBUG:
-        return
-    print(str, end=end)
+import logging
 
 class Player:
     """
@@ -24,50 +18,52 @@ class Player:
         self._tags = {}
 
     def decode_src_created(self, element, new_pad, data):
-        debug("Received new pad '{0}' from '{1}'".format(
+        logging.debug("Received new pad '{0}' from '{1}'".format(
             new_pad.get_name(), element.get_name()))
 
         if new_pad.is_linked():
-            debug("  We are already linked. Ignoring.")
+            logging.debug("  We are already linked. Ignoring.")
             return None
 
         new_pad_type = new_pad.query_caps(None).to_string()
         if not new_pad_type.startswith("audio/x-raw"):
-            debug("  It has type '{0}' which is not raw audio. Ignoring.".format(
+            logging.debug("  It has type '{0}' which is not raw audio. Ignoring.".format(
                 new_pad_type))
             return None
 
         audiopad = data.get_static_pad('sink')
         ret = new_pad.link(audiopad)
         if ret is not Gst.PadLinkReturn.OK:
-            debug("  Failed to link pad: {0}".format(ret))
+            logging.warning("  Failed to link pad: {0}".format(ret))
+            # todo: how to handle this??
 
     def handle_message(self, bus, message):
         if message.type == Gst.MessageType.EOS:
             # this shouldn't happen with our internet radio streams..
-            debug("End-of-stream")
+            logging.warning("End-of-stream occurred, shouldn't happen with live stream")
             return False
         elif message.type == Gst.MessageType.BUFFERING:
             percent = message.parse_buffering()
-            debug("Buffering {0}%   ".format(percent), '\r')
+            logging.debug("Buffering {0}%   ".format(percent))
+            # todo: callback to let user know we're buffering
 
             # don't try to pause/resume for live streams, will fail
             if not self._is_live:
                 # wait until buffering is complete before start/resume playing
                 if (percent < 100):
-                    debug("Pausing, waiting for data")
+                    logging.debug("Pausing, waiting for data")
                     self._pipe.set_state(Gst.State.PAUSED)
                 else:
-                    debug("Resuming")
+                    logging.debug("Resuming")
                     self._pipe.set_state(Gst.State.PLAYING)
         elif message.type == Gst.MessageType.CLOCK_LOST:
-            debug("Lost clock, recovering stream")
+            logging.debug("Lost clock, recovering stream")
             self._pipe.set_state(Gst.State.PAUSED)
             self._pipe.set_state(Gst.State.PLAYING)
         elif message.type == Gst.MessageType.STATE_CHANGED:
             if message.src.get_name() == 'pipeline':
                 old_state, new_state, pending_state = message.parse_state_changed()
-                debug("Pipeline changed from {0} to {1}.".format(
+                logging.debug("Pipeline changed from {0} to {1}.".format(
                     old_state.value_nick, new_state.value_nick))
         elif message.type == Gst.MessageType.TAG:
             tag_list = message.parse_tag()
@@ -77,19 +73,21 @@ class Player:
                     value = str(tag_list.get_value_index(tag, i))
                     if tag not in self._tags or self._tags[tag] != value:
                         self._tags[tag] = value
-                        debug("{0}: {1}".format(tag, value))
+                        logging.debug("{0}: {1}".format(tag, value))
+                        # todo: callback to update UX 
         elif message.type == Gst.MessageType.WARNING:
             err, msg = message.parse_warning()
-            debug("Warning: {0}: {1}".format(err, msg))
+            logging.warning("Warning: {0}: {1}".format(err, msg))
         elif message.type == Gst.MessageType.ERROR:
             err, msg = message.parse_error()
-            debug("Error: {0}: {1}".format(err, msg))
+            logging.warning("Error: {0}: {1}".format(err, msg))
+            # todo: not sure when this happens, but need to handle it?
             return False
         return True
 
     def create_pipeline(self, uri, is_live=False):
         # valid uri only, not filename
-        debug("Attempting to play {0}".format(uri))
+        logging.debug("Attempting to play {0}".format(uri))
         self._is_live = is_live
 
         pipe = Gst.Pipeline.new('pipeline')
@@ -127,13 +125,17 @@ class Player:
         pipe.add(decode)
         queue.link(decode)
 
+        # todo: should split out all the .add to a for in [] to handle None response
+        # todo: don't think that .link can fail until pipeline starts, but should handle
+
         # start play back and listed to events
         ret = pipe.set_state(Gst.State.PLAYING)
         if ret == Gst.StateChangeReturn.FAILURE:
-            debug("Unable to set the pipeline to the playing state")
+            logging.warning("Unable to set the pipeline to the playing state")
+            # todo: not sure when this happens, but confirm this is handled elegently
             return False
         elif ret == Gst.StateChangeReturn.NO_PREROLL:
-            debug("Stream is live")
+            logging.debug("Stream is live")
             self._is_live = True
 
         self._running = True
@@ -144,7 +146,8 @@ class Player:
 
     def _process_loop(self):
         bus = self._pipe.get_bus()
-        while self._running: # need some sync
+        # todo: check all this is thread safe!
+        while self._running:
             message = bus.timed_pop(100 * Gst.MSECOND)
             if message is not None:
                 if not self.handle_message(bus, message):
@@ -154,7 +157,7 @@ class Player:
 
     def destroy_pipeline(self):
         if self._pipe is not None:
-            debug("Stopping pipeline")
+            logging.debug("Stopping pipeline")
             self._pipe.set_state(Gst.State.NULL)
             self._running = False
             self._thread.join()
