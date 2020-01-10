780e544fbb568777168b4e7f1076762b0ed8488a
Alex Linde <alex@somefamilies.com>
2020-01-10 13:32:57 -0800

    use standard python logging, mpeg-dash streams

--- test-player.py
+++ test-player.py
@@ -1,10 +1,22 @@
 from player import Player
-import time
+import time, logging
 
-URI = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"
-
+logging.basicConfig(level=logging.DEBUG)
+# bitrate vlow / low / med / high as per uri, only vlow and low outside UK
+#URI = "http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_one/format/pls.pls" # MP3, 128k
+#URI = "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p" # MP3, 128k
+#URI = "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/llnw/bbc_radio_one.m3u8" # HLS Stream, 96k
+URI = "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnw/bbc_radio_one.mpd" # MPEG-DASH, 96k
 p = Player()
 
-p.create_pipeline(URI,True)
-time.sleep(10)
-p.destroy_pipeline()
+try:
+    p.create_pipeline(URI,True)
+
+    while True:
+        time.sleep(10)
+
+except (KeyboardInterrupt, SystemExit):
+    pass
+
+finally:
+    p.destroy_pipeline()
